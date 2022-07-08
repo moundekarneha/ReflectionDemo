a68b67d8c912ee0ce1a977fa96e19015a5cce786
HP <HP@DESKTOP-G4J7E8F>
2022-07-08 18:14:22 +0530

    Add project files.

---
+++ ReflectionDemo/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace ReflectionDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"E:\Projects\All_prac\C#\Lamda Expression\LamdaExpressionDemo\LamdaExpressionDemo\bin\Debug \LamdaExpressionDemo.exe";        
+        
+            Assembly assembly = Assembly.LoadFile(path);
+            Type[] types = assembly.GetTypes(); 
+
+            foreach(var type in types)
+            {
+                Console.WriteLine("Class Name: "+type.Name);
+
+                MethodInfo[] methods = type.GetMethods();
+                foreach (var method in methods)
+                {
+                    Console.WriteLine("Method Name: " + method.Name);
+
+                    ParameterInfo[] parameters = method.GetParameters();
+                    foreach (var para in parameters)
+                    {
+                        Console.WriteLine("Parameter Name: " + para.Name+" Parameter Type : "+para.ParameterType);
+                    }
+                }
+
+            }
+            Console.ReadLine();
+        
+        
+        
+        }
+    }
+}
